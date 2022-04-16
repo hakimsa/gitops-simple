fb067a69f29705005a15182929daedb6f265efb3
Kim <samouh591@gmail.com>
2022-04-17 00:50:13 +0200

    testing

--- midlewares/errorHandlers.js
+++ midlewares/errorHandlers.js
@@ -2,10 +2,9 @@ const {logEvents} = require("./LogEvents");
 
 
 
-/*
 const errorHandeler=(res,req,err,next)=>{
   logEvents( err.name +"\t" +"\t"+"Error" +"\t"+err.message+"\t");
  res.status(500).send(err.message);
     next()
 };
-  module.exports=errorHandeler;*/
+  module.exports=errorHandeler;
