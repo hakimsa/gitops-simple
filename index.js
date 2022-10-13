d2bd8100c5d3a4ea223c890b749adbfcba4dc169
Kim <samouh591@gmail.com>
2022-10-13 16:47:02 +0200

    modified files

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-﻿const express = require('express');
+﻿﻿const express = require('express');
 const bodyParser = require('body-parser');
-const ProductoController = require('./controllers/ProductoController');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 8088;
 const app = express();
 const {logger,logEvents}=require("./midlewares/LogEvents");
-const errorHandeler =require("./midlewares/errorHandlers");
 const {serv,user,pass,cluster,db}=require("./dbConfig/conf");
 const rooter = require('./roots/api/productos');
-const roote = require('./roots/root');
-const path=require("path");
+const index= require('./roots/root');
 app.use(bodyParser.urlencoded( {extended:false} ));
 app.use(bodyParser.json());
 const conexion=(serv+user+pass+cluster+db+"?retryWrites=true");
@@ -27,22 +24,8 @@ app.use( (req, res, next) => {
 app.use(logger);
 //app.use(errorHandeler);
 app.use(rooter);
-app.all('*',(req,res)=>{
-	res.status(404);
-if (req.accepts("html")){
-	res.sendFile(path.join(__dirname,"views","404.html"))
+app.use(index);
 
-}else if (req.accepts("json")){
-
-	res.json({
-
-		"error":"404 Not found"});
-}else {
-	res.type("txt").send("404 Not found");
-
-}
-}
-);
 mongoose.connect(conexion.toString(),
  { useNewUrlParser: true, useFindAndModify:false }).then(
     ()=> {  
