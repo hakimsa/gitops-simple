2fb53236bd4bfbd5e44a012bc16c006055edbcf3
hakim <hakimsa>
2022-11-29 08:52:50 +0100

    delete --cached file

--- dbConfig/conf.js
+++
@@ -1,6 +0,0 @@
-const user="hakim:";
-const pass="Ad1234";
-const cluster="@cluster0-y69ki.mongodb.net/";
-const db="productos";
-const serv="mongodb+srv://";
-module.exports={user,pass,cluster,db,serv}
