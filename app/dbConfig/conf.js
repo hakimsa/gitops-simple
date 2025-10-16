eb7073a52821839e8de52feda93df1f1fd40b4df
hakim <samouh591@gmail.com>
2025-10-16 21:36:30 +0200

    fix: ci-cd workflow to Cloud Run

---
+++ app/dbConfig/conf.js
@@ -0,0 +1,7 @@
+const user = "admin";
+const pass = "Cafe2020";
+const cluster = "cluster0.9pawl.mongodb.net";
+const db = "dbtest";
+const serv = "mongodb+srv://";
+
+module.exports={user,pass,cluster,db,serv};
