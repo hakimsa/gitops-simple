79723f405ab7c5c5e8f2bde078ffd563d1e581c8
hakim <samouh591@gmail.com>
2025-10-30 14:56:52 +0100

    add sonarQube

--- infra/sa.sh
+++ infra/sa.sh
@@ -22,3 +22,4 @@ gcloud projects add-iam-policy-binding fiery-synthesis-472821-m2 \
 gcloud projects add-iam-policy-binding fiery-synthesis-472821-m2 \
   --member="serviceAccount:sa-cloudbuild@fiery-synthesis-472821-m2.iam.gserviceaccount.com" \
   --role="roles/iam.serviceAccountUser"
+  08c2881654c750c8d1ee78796ba42ac8f6adf36c
