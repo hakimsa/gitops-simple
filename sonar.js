b2fe51d120f07cb57b557bb1cee65765c5cf5943
hakim <samouh591@gmail.com>
2025-11-03 23:03:07 +0100

    add conf dev

--- sonar.js
+++ sonar.js
@@ -4,7 +4,7 @@ const scanner = require('sonarqube-scanner');
 scanner({
   serverUrl: 'https://sonarcloud.io',
   options: {
-    'sonar.projectKey': 'hakimsa_devops-repo',
+    'sonar.projectKey': 'hakimsa_gitops-simple',
     'sonar.organization': 'hakimsa',
     'sonar.sources': '.',
   }
