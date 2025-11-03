3ca8c6826fde41d647b8fd63e0bd58f8adec9741
hakim <samouh591@gmail.com>
2025-11-03 19:55:53 +0100

    add conf dev

--- tres.sh
+++ tres.sh
@@ -1,17 +1,43 @@
-PROJECT_ID=fiery-synthesis-472821-m2
-SA=sa-cloudbuild@$PROJECT_ID.iam.gserviceaccount.com
 
-# Permitir usar Cloud Build
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  PROJECT_ID="fiery-synthesis-472821-m2"
+SA="serviceAccount:github-actions-sa@$PROJECT_ID.iam.gserviceaccount.com"
+
+# Permitir lanzar builds y ver resultados
 gcloud projects add-iam-policy-binding $PROJECT_ID \
-  --member="serviceAccount:$SA" \
+  --member="$SA" \
   --role="roles/cloudbuild.builds.editor"
 
-# Permitir acceso al bucket interno de Cloud Build
+# Permitir usar Artifact Registry (subir imágenes Docker)
 gcloud projects add-iam-policy-binding $PROJECT_ID \
-  --member="serviceAccount:$SA" \
-  --role="roles/storage.admin"
+  --member="$SA" \
+  --role="roles/artifactregistry.writer"
 
-# Permitir usar servicios en general
+# Permitir desplegar en Cloud Run
 gcloud projects add-iam-policy-binding $PROJECT_ID \
-  --member="serviceAccount:$SA" \
-  --role="roles/serviceusage.serviceUsageConsumer"
+  --member="$SA" \
+  --role="roles/run.admin"
+
+# Permitir autenticarse y gestionar cuentas de servicio
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="$SA" \
+  --role="roles/iam.serviceAccountUser"
+
+# (Opcional, pero útil para leer logs de build)
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="$SA" \
+  --role="roles/viewer"
